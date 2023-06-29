20e7b32c6161ac03df674974ddf5a116f5a2f006
moamel-hashim <muamal1997@gmail.com>
2023-06-28 22:03:21 -0700

    completed hw

---
+++ main.js
@@ -0,0 +1,3 @@
+const firstNumber = Number(window.prompt('enter a number'));
+const secondNumber = Number(window.prompt('enter a second number'))
+window.alert(`the total for the numbers you've entered is ${firstNumber + secondNumber}`)
